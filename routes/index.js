88d59c48e3bcaeb030b7ca634e522be1b67197d5
karl_martin_miil <kmmiil95@gmail.com>
2016-03-27 15:55:27 +0300

    auto deploy

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,18 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.post('/deploy/', function (req, res) {
+  var spawn = require('child_process').spawn,
+      deploy = spawn('sh', [ '../deploy.sh' ]);
+
+  deploy.stdout.on('data', function (data) {
+    console.log(''+data);
+  });
+
+  deploy.on('close', function (code) {
+    console.log('Child process exited with code ' + code);
+  });
+  res.json(200, {message: 'Github Hook received!'})
+});
+
 module.exports = router;
